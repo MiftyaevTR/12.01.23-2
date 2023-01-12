f25f7c9a9095a59859d427daa561701f6260f7fb
Timur <t926661227@yandex.ru>
2023-01-12 13:10:09 +0300

    Первые две задачи решены

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+int GetStepen (int A, int B)
+{
+    int res =1;
+    for (int el =1; el<=B; el++)
+    {
+        res=res*A;
+    }
+    return res;
+}
+
+Console.Write("Просьба введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Просьба введите степень: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = GetStepen(num1,num2);
+Console.WriteLine(result);
+*/
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+int GetSum (int num)
+{
+    int sum =0;
+    for (int el=0; num>0; el++)
+    {
+        sum=sum+num%10;
+        num=(num-num%10)/10;
+    }
+    return sum;
+}
+
+Console.Write("Просьба введите число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int result = GetSum(number);
+Console.WriteLine(result);
+*/
